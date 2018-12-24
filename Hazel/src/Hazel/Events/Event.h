3b4615a22dcaba8bd00d0c4e54a1fc3254cb2801
Brandon Byrd <byrdbrandon7@gmail.com>
2018-12-23 22:18:04 -0500

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel/Events/Event.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Hazel/Core.h"
+
+#include <string>
+#include <functional>
+
+namespace Hazel {
+	
+	enum class EventType
+	{
+		None = 0,
+		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		AppTick, AppUpdate, AppRender,
+		KeyPressed, KeyReleased,
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
+
+	enum EventCategory
+	{
+		None = 0,
+		EventCategoryApplication  = BIT(0),
+		EventCategoryInput        = BIT(1),
+		EventCategoryKeyboard     = BIT(2),
+		EventCategoryMouse        = BIT(0),
+	};
+}
